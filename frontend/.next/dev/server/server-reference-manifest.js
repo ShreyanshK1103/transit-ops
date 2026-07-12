703d70410b5985f1068f1bf26fc1cb78f2a8121b
ShreyanshK1103 <shreyanskaushal1103@gmail.com>
2026-07-12 17:24:26 +0530

    Stop tracking .next

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KMdE9mrzSXN2E/TYODWORaO7bJuxk6zJk4ZVmP/pdFU=\"\n}"
--- frontend/.next/dev/static/chunks/app_page_tsx_1mizu_5._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_086pn6x._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_recharts_es6_util_0bimyet._.js","static/chunks/node_modules_recharts_es6_state_0_v2dfg._.js","static/chunks/node_modules_recharts_es6_component_1cy0ayp._.js","static/chunks/node_modules_recharts_es6_cartesian_13auwka._.js","static/chunks/node_modules_recharts_es6_1vp7ic7._.js","static/chunks/node_modules_1cmr6e9._.js"],
-    source: "dynamic"
-});
